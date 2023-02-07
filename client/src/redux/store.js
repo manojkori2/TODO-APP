4eb58aa469e8092d9562688aba42eccf21cb8f57
manojkori2 <122771243+manojkori2@users.noreply.github.com>
2023-02-07 12:40:29 +0530

    Add files via upload

---
+++ client/src/redux/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { todoReducers } from './reducers/todosReducer';
+
+
+const reducer = combineReducers({
+    todos: todoReducers
+})
+
+const middleware = [thunk];
+const store = createStore(
+    reducer,
+    composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store;
